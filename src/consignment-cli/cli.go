d1c1876c8a22494c82ce3d29bba4fa54ea404680
dsl <m15576684447@163.com>
2019-07-28 22:38:27 +0800

    feat: add vender dir

---
+++ src/consignment-cli/cli.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/config/cmd"
+	"io/ioutil"
+	pb "learn/shippy/src/consignment-service/proto/consignment"
+	"log"
+	"os"
+	"github.com/micro/go-micro/metadata"
+)
+
+const (
+	ADDRESS           = "localhost:50051"
+	DEFAULT_INFO_FILE = "consignment.json"
+)
+
+// 读取 consignment.json 中记录的货物信息
+func parseFile(fileName string) (*pb.Consignment, error) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		return nil, err
+	}
+	var consignment *pb.Consignment
+	err = json.Unmarshal(data, &consignment)
+	if err != nil {
+		return nil, errors.New("consignment.json file content error")
+	}
+	return consignment, nil
+}
+
+func main() {
+
+	cmd.Init()
+
+	// 创建微服务的客户端，简化了手动 Dial 连接服务端的步骤
+	client := pb.NewShippingServiceClient("go.micro.srv.consignment", microclient.DefaultClient)
+
+	// 在命令行中指定新的货物信息 json 件
+	if len(os.Args) < 3 {
+		log.Fatalln("Not enough arguments, expecing file and token.")
+	}
+	infoFile := os.Args[1]
+	token := os.Args[2]
+
+	// 解析货物信息
+	consignment, err := parseFile(infoFile)
+	if err != nil {
+		log.Fatalf("parse info file error: %v", err)
+	}
+
+	// 创建带有用户 token 的 context
+	// consignment-service 服务端将从中取出 token，解密取出用户身份
+	tokenContext := metadata.NewContext(context.Background(), map[string]string{
+		"token": token,
+	})
+
+	// 调用 RPC
+	// 将货物存储到指定用户的仓库里
+	resp, err := client.CreateConsignment(tokenContext, consignment)
+	if err != nil {
+		log.Fatalf("create consignment error: %v", err)
+	}
+	log.Printf("created: %t", resp.Created)
+
+	// 列出目前所有托运的货物
+	resp, err = client.GetConsignments(tokenContext, &pb.GetRequest{})
+	if err != nil {
+		log.Fatalf("failed to list consignments: %v", err)
+	}
+	for i, c := range resp.Consignments {
+		log.Printf("consignment_%d: %v\n", i, c)
+	}
+}
